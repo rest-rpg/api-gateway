c60e424cbdfa614167f29d86cd9e87c4ff40250c
MichalSulima1999 <michal.sulima1999@gmail.com>
2024-12-21 16:17:09 +0100

    Migration to Spring Cloud Gateway

--- src/main/java/com/microservices/apigateway/routes/Routes.java
+++ src/main/java/com/microservices/apigateway/routes/Routes.java
@@ -1,51 +1,39 @@
 package com.microservices.apigateway.routes;
 
-import org.springframework.cloud.gateway.server.mvc.filter.CircuitBreakerFilterFunctions;
-import org.springframework.cloud.gateway.server.mvc.handler.GatewayRouterFunctions;
-import org.springframework.cloud.gateway.server.mvc.handler.HandlerFunctions;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpStatus;
-import org.springframework.web.servlet.function.RequestPredicates;
-import org.springframework.web.servlet.function.RouterFunction;
-import org.springframework.web.servlet.function.ServerResponse;
-
-import java.net.URI;
 
 @Configuration
 public class Routes {
 
     @Bean
-    public RouterFunction<ServerResponse> gameServiceRoute() {
-        return GatewayRouterFunctions.route("game_service")
-                .route(RequestPredicates.path("/game/**"), HandlerFunctions.http("http://localhost:8080"))
-                .filter(CircuitBreakerFilterFunctions.circuitBreaker("gameServiceCircuitBreaker",
-                        URI.create("forward:/fallbackRoute")))
+    public RouteLocator gameServiceRoute(RouteLocatorBuilder builder) {
+        return builder.routes()
+                .route("game-route", r -> r.path("/game/**")
+                        .filters(f -> f.stripPrefix(1))
+//                                .circuitBreaker(c -> c.setName("gameServiceCircuitBreaker")
+//                                        .setFallbackUri("forward:/fallbackRoute")))
+                        .uri("http://localhost:8080"))
                 .build();
     }
 
     @Bean
-    public RouterFunction<ServerResponse> userServiceRoute() {
-        return GatewayRouterFunctions.route("user_service")
-                .route(RequestPredicates.path("/user/**"), HandlerFunctions.http("http://localhost:8081"))
-                .filter(CircuitBreakerFilterFunctions.circuitBreaker("userServiceCircuitBreaker",
-                        URI.create("forward:/fallbackRoute")))
+    public RouteLocator userServiceRoute(RouteLocatorBuilder builder) {
+        return builder.routes()
+                .route("user-route", r -> r.path("/user/**")
+                        .filters(f -> f.stripPrefix(1))
+                        .uri("http://localhost:8081"))
                 .build();
     }
 
     @Bean
-    public RouterFunction<ServerResponse> authServiceRoute() {
-        return GatewayRouterFunctions.route("auth_service")
-                .route(RequestPredicates.path("/auth/**"), HandlerFunctions.http("http://localhost:8082"))
-                .filter(CircuitBreakerFilterFunctions.circuitBreaker("authServiceCircuitBreaker",
-                        URI.create("forward:/fallbackRoute")))
-                .build();
-    }
-
-    @Bean
-    public RouterFunction<ServerResponse> fallbackRoute() {
-        return GatewayRouterFunctions.route("fallbackRoute")
-                .GET("/fallbackRoute", request -> ServerResponse.status(HttpStatus.SERVICE_UNAVAILABLE).body("Service Unavailable, please try again later"))
+    public RouteLocator authServiceRoute(RouteLocatorBuilder builder) {
+        return builder.routes()
+                .route("auth-route", r -> r.path("/auth/**")
+                        .filters(f -> f.stripPrefix(1))
+                        .uri("http://localhost:8082"))
                 .build();
     }
 }
